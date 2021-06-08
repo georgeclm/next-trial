34d2af6ea0f7ec2bfa822d28be1ccfb02adac467
George <cavidjaja@gmail.com>
2021-06-08 22:07:44 +0700

    up

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== "production";
 export const server = dev
   ? "http://localhost:3000"
-  : "https://jsonplaceholder.typicode.com";
+  : "https://next-trial-ten.vercel.app";
